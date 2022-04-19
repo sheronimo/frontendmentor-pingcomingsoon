b0042fe9e40a0c035584b2944b1639d303ccf20a
Shuber <shuberinu@gmail.com>
2022-04-19 13:50:38 +0700

    form validation with error message

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const email = document.querySelector('#email');
+const form = document.querySelector('#subscribe-form');
+const error = document.querySelector('#error');
+
+form.addEventListener('submit', (e) => {
+	let errorMessage = '';
+
+	// regex for email format
+	const pattern = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+	if (email.value === '' || email.value == null) {
+		errorMessage = 'Email address cannot be left blank.';
+	} else if (!pattern.test(email.value)) {
+		errorMessage = 'Please provide a valid email address.';
+	}
+
+	if (errorMessage.length > 0) {
+		e.preventDefault();
+		error.innerText = errorMessage;
+		email.style.borderColor = 'hsl(354, 100%, 66%)';
+	}
+});
